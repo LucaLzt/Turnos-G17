ef7de07255f7993a2412e5be14d87c013d0bb4be
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-05-22 15:11:43 -0300

    feat: se agrega configuracion modelmapper

---
+++ src/main/java/com/oo2/grupo17/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.oo2.grupo17.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		
+		return mapper;
+	}
+	
+}
