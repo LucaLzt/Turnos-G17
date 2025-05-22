ac49b514557b455b51fbddcbc50336b8aec19a53
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-05-22 16:19:45 -0300

    feat: se eliminan lineas de configuración

--- src/main/java/com/oo2/grupo17/config/ModelMapperConfig.java
+++ src/main/java/com/oo2/grupo17/config/ModelMapperConfig.java
@@ -1,8 +1,15 @@
 package com.oo2.grupo17.config;
 
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.oo2.grupo17.dtos.TareaDto;
+import com.oo2.grupo17.entities.Profesional;
+import com.oo2.grupo17.entities.Tarea;
 
 @Configuration
 public class ModelMapperConfig {
@@ -10,7 +17,10 @@ public class ModelMapperConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper mapper = new ModelMapper();
-		
+		mapper.getConfiguration()
+			.setMatchingStrategy(MatchingStrategies.STRICT)
+			.setFieldMatchingEnabled(true)
+			.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
 		return mapper;
 	}
 	
