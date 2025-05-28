0515021e19e0630ecbd59235b4a3c3960cb66bac
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-05-28 02:42:20 -0300

    feat: se agrega el jpa para disponibilidad

---
+++ src/main/java/com/oo2/grupo17/repositories/IDisponibilidadRepository.java
@@ -0,0 +1,11 @@
+package com.oo2.grupo17.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oo2.grupo17.entities.Disponibilidad;
+
+public interface IDisponibilidadRepository extends JpaRepository<Disponibilidad, Long> {
+	
+	// Ver que poner
+	
+}
