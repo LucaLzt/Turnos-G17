8d493f32680b18b79616b5fc221b938373dede53
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-05-28 23:56:05 -0300

    feat: se cambia la relacion de persona, contacto, direccion a 1:1

--- src/main/java/com/oo2/grupo17/entities/Direccion.java
+++ src/main/java/com/oo2/grupo17/entities/Direccion.java
@@ -1,16 +1,33 @@
 package com.oo2.grupo17.entities;
 
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-@Embeddable
+@Entity
+@Table(name = "Direccion")
 public class Direccion {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @Setter(AccessLevel.PROTECTED) Long id;
+	
+	@Column(name = "calle")
 	private String calle;
+	
+	@Column(name = "altura")
 	private int altura;
+	
+	/*
 	private String localidad;
 	private String provincia;
+	*/
 	
 }
--- src/main/java/com/oo2/grupo17/entities/Lugar.java
+++ src/main/java/com/oo2/grupo17/entities/Lugar.java
@@ -6,13 +6,14 @@ import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,7 +27,8 @@ public class Lugar {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Embedded
+	@OneToOne
+	@JoinColumn(name = "direccion_id")
 	private Direccion direccion;
 	
 	@Column(name = "horario_apertura", nullable = false)
