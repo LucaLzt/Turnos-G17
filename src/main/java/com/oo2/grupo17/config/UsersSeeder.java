77a3fd60dcef6fa977e8107abb8b7313ac0049e4
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-05-30 00:06:26 -0300

    feat: se arregla error al crear roles

---
+++ src/main/java/com/oo2/grupo17/config/UsersSeeder.java
@@ -0,0 +1,68 @@
+package com.oo2.grupo17.config;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.oo2.grupo17.entities.RoleEntity;
+import com.oo2.grupo17.entities.RoleType;
+import com.oo2.grupo17.entities.UserEntity;
+import com.oo2.grupo17.repositories.IRoleRepository;
+import com.oo2.grupo17.repositories.IUserRepository;
+
+import java.util.Set;
+
+@Component
+public class UsersSeeder implements CommandLineRunner {
+
+    private final IUserRepository userRepository;
+    private final IRoleRepository roleRepository;
+
+    public UsersSeeder(IUserRepository userRepository, IRoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadRoles();
+        loadUsers();
+    }
+
+    private void loadUsers() {
+        if (userRepository.count() == 0){
+            loadUserAdmin();
+        }
+    }
+    
+    private void loadUserAdmin() {
+        userRepository.save(buildUserAdmin("admin1234@admin.com", "admin1234"));
+    }
+    
+    private UserEntity buildUserAdmin(String username, String password) {
+    	return UserEntity.builder()
+    			.username(username)
+    			.active(true)
+    			.password(encryptPassword(password))
+                .roleEntities(Set.of(roleRepository.findByType(RoleType.ADMIN).get()))
+                .build();
+    }
+
+    private void loadRoles() {
+        if (roleRepository.count() == 0){
+            roleRepository.save(buildRole(RoleType.CLIENTE));
+            roleRepository.save(buildRole(RoleType.ADMIN));
+            roleRepository.save(buildRole(RoleType.PROFESIONAL));
+        }
+    }
+
+    private RoleEntity buildRole(RoleType roleType) {
+        return RoleEntity.builder()
+                .type(roleType)
+                .build();
+    }
+
+    private String encryptPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(7);
+        return passwordEncoder.encode(password);
+    }
+}
