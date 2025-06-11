ec3b8d72cf610d746828482a0b1d2c37738ee026
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-06-11 17:59:39 -0300

    se termina de trabajar en la organización de archivos

--- src/main/java/com/oo2/grupo17/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/oo2/grupo17/exceptions/GlobalExceptionHandler.java
@@ -5,42 +5,42 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionException {
+public class GlobalExceptionHandler {
 
 	@ExceptionHandler(EntidadNoEncontradaException.class)
 	public String handleEntidadNoEncontrada(EntidadNoEncontradaException ex, Model model) {
 		model.addAttribute("mensaje", ex.getMessage());
-		return "error-personalizados/entidad-no-encontrada";
+		return "error/personalizados/entidad-no-encontrada";
 	}
 	
     @ExceptionHandler(DniIncorrectoException.class)
     public String handleDniIncorrecto(DniIncorrectoException ex, Model model) {
         model.addAttribute("mensaje", ex.getMessage());
-        return "error-personalizados/credencial-incorrecta";
+        return "error/personalizados/credencial-incorrecta";
     }
 
     @ExceptionHandler(ContraseñaIncorrectaException.class)
     public String handleContraseñaIncorrecta(ContraseñaIncorrectaException ex, Model model) {
         model.addAttribute("mensaje", ex.getMessage());
-        return "error-personalizados/credencial-incorrecta";
+        return "error/personalizados/credencial-incorrecta";
     }
 
     @ExceptionHandler(EmailIncorrectoException.class)
     public String handleEmailIncorrecto(EmailIncorrectoException ex, Model model) {
         model.addAttribute("mensaje", ex.getMessage());
-        return "error-personalizados/credencial-incorrecta";
+        return "error/personalizados/credencial-incorrecta";
     }
     
     @ExceptionHandler(RolNoEncontradoException.class)
     public String handleEmailIncorrecto(RolNoEncontradoException ex, Model model) {
         model.addAttribute("mensaje", ex.getMessage());
-        return "error-personalizados/rol-no-encontrado";
+        return "error/personalizados/rol-no-encontrado";
     }
     
     @ExceptionHandler(EntidadDuplicadaException.class)
     public String manejarEntidadDuplicada(EntidadDuplicadaException ex, Model model) {
         model.addAttribute("errorDuplicado", ex.getMessage());
-        return "error-personalizados/elemento-duplicado";
+        return "error/personalizados/elemento-duplicado";
     }
 	
 }
