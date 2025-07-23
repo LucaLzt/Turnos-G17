e92c3325bd08dd0f22d7699db74a70c8e85a1f00
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-07-23 20:06:50 -0300

    feat: agrega configuración de seguridad básica en Swagger

--- src/main/java/com/oo2/grupo17/config/SwaggerConfig.java
+++ src/main/java/com/oo2/grupo17/config/SwaggerConfig.java
@@ -3,10 +3,12 @@ package com.oo2.grupo17.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,6 +22,14 @@ public class SwaggerConfig {
 					.description("API documentation for Turnos G17 application")
 					.contact(new Contact().name("Grupo 17"))
 					.license(new License().name("MIT").url("https://opensource.org/license/mit/"))
-					);
+					)
+				.components(
+						new Components()
+		                	.addSecuritySchemes("basicAuth", 
+		                			new SecurityScheme()
+		                            .type(SecurityScheme.Type.HTTP)
+		                            .scheme("basic")
+		                    )
+		            );
 	}
 } 
